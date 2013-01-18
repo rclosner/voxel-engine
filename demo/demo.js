a75f6521700d9674d9dee8d4f47cd441c1503a21
Ryan Closner <ryanc@codeforamerica.org>
2013-01-17 19:36:40 -0800

    temp

--- demo/demo.js
+++ demo/demo.js
@@ -2,32 +2,83 @@ var createGame = require('../lib/game')
 var THREE = require('three')
 var voxel = require('voxel')
 var toolbar = require('toolbar')
+var chunkSize = 32;
+var chunkDistance = 2;
+var width = chunkSize * 2 * chunkDistance;
 window.blockSelector = toolbar({el: '#tools'})
 var skin = require('minecraft-skin')
 
-var generator = function(low, high, x, y, z) {
-  var chunkIndex = [x, y, z].join('|')
-  var chunk = this.chunks[chunkIndex]
-  var voxels
-  if (chunk) voxels = chunk.voxels
-  return voxel.generate(low, high, function(vx, vy, vz, n) {
-    if (voxels) return voxels[n]
-    return voxel.generator['Valley'](vx, vy, vz)
-  })
+var ORIGIN = {
+  latitude:  37.77559,
+  longitude: -122.41391
+}
+
+var GEO_NODES = [
+  { latitude: 37.77559, longitude: -122.41391, elevation: 0.7 },
+  { latitude: 37.7747870, longitude: -122.4083100, elevation: 0.1 },
+  { latitude: 37.771, longitude: -122.4073, elevation: 0.1 }
+]
+
+var LATITUDE_TO_METERS  = 110992;
+var LONGITUDE_TO_METERS = 88099;
+
+function scale( x, fromLow, fromHigh, toLow, toHigh ) {
+  return ( x - fromLow ) * ( toHigh - toLow ) / ( fromHigh - fromLow ) + toLow
+}
+
+var geo_to_geom = function (node) {
+  var x = (ORIGIN.latitude - node.latitude) * LATITUDE_TO_METERS * 10000;
+  var z = (ORIGIN.longitude - node.longitude) * LONGITUDE_TO_METERS * 10000;
+  x = scale(x, -180 * LATITUDE_TO_METERS, 180 * LATITUDE_TO_METERS, 0,  width);
+  z = scale(z, -180 * LONGITUDE_TO_METERS, 180 * LONGITUDE_TO_METERS, 0, width);
+
+  return { x: Math.floor(x), y: node.elevation, z: Math.floor(z) }
+}
+
+window.NODES = new Int8Array(width * width);
+
+for (var i = 0; i < GEO_NODES.length; i++) {
+  var node  = geo_to_geom(GEO_NODES[i]);
+  var index = node.x + node.z * width;
+  console.log(index)
+  var y = ~~scale(node.y, 0, 1, 0, width)
+  console.log(y)
+  window.NODES[index] = y;
+}
+
+var fromLow = chunkSize * -chunkDistance;
+var toHigh  = chunkSize * chunkDistance;
+
+var osm_generator = function (x,y,z,n) {
+  x = scale(x, fromLow, toHigh, 0, width);
+  z = scale(z, fromLow, toHigh, 0, width);
+  y = scale(y, fromLow, toHigh, 0, width);
+
+  var index = x + z * width
+
+  if (NODES[index]) {
+    var node = NODES[index];
+    if (node == y) return 1; 
+  } else {
+    return 0; 
+  }
 }
 
 window.game = createGame({
-  generateVoxelChunk: generator,
+  generate: osm_generator,
   texturePath: '/textures/',
   materials: ['grass', 'brick', 'dirt', 'obsidian', 'crate'],
   cubeSize: 25,
-  chunkSize: 32,
-  chunkDistance: 2,
+  chunkSize: chunkSize,
+  chunkDistance: chunkDistance,
   startingPosition: [35, 100, 35],
   worldOrigin: [0,0,0],
   controlOptions: {jump: 6}
 })
 
+window.game.on("tick", function () {
+  game.controls.gravityEnabled = false;
+});
 window.viking = skin(game.THREE, 'viking.png').createPlayerObject()
 viking.position.y = 60
 game.scene.add(viking)
